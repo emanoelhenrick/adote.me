6899679a47e4214b5488caf6d71aa63f94c9497d
injuje <jlvrs@cesar.school>
2025-05-21 03:21:41 -0300

    add-volunteers.py

--- src/data/volunteers.py
+++ src/data/volunteers.py
@@ -1,21 +1,87 @@
+import os
+import json
 from uuid import uuid4
+from src.data.utils import getJsonPath
+
+VOLUNTEER_PATH = getJsonPath('volunteers')
 
 def create(volunteer):
     new_uuid = uuid4().__str__()
     volunteer['id'] = new_uuid
+
+    if not os.path.exists(VOLUNTEER_PATH):
+        with open(VOLUNTEER_PATH, 'w') as file:
+            json.dump([volunteer], file, indent=4)
+        return volunteer
+    
+    with open(VOLUNTEER_PATH, 'r') as file:
+        volunteer_json = json.load(file)
+        volunteer_json.append(volunteer)
+
+    with open(VOLUNTEER_PATH, 'w') as file:
+        json.dump(volunteer_json, file, indent=4)
+
     return volunteer
 
+
+
 def update(updated_volunteer):
-    print("altera uma entidade")
+    if not os.path.exists(VOLUNTEER_PATH):
+        return None
+    
+    with open(VOLUNTEER_PATH, 'r') as file:
+        volunteer_json = json.load(file)
+
+        for volunteer in volunteer_json:
+            if volunteer['id'] == updated_volunteer['id']:
+                volunteer.update(updated_volunteer)
+                break
+        
+        with open(VOLUNTEER_PATH, 'w') as file:
+            json.dump(volunteer_json, file, indent=4)
+
+    return updated_volunteer
+
+
 
 def readById(volunteer_id):
-    print("lê uma entidade")
+    if not os.path.exists(VOLUNTEER_PATH):
+        return None
+    
+    with open(VOLUNTEER_PATH, 'r') as file:
+        volunteer_json = json.load(file)
+
+        for volunteer in volunteer_json:
+            if volunteer["id"] == volunteer_id:
+                return volunteer
+    
+    return None
+
+
 
 def readAll():
-    print("lê todas as entidades")
+   if not os.path.exists(VOLUNTEER_PATH):
+       return None
+   
+   with open(VOLUNTEER_PATH, 'r') as file:
+       volunteer_json = json.load(file)
+       return volunteer_json
 
-def readByShelterId(shelter_id):
-    print("lê todas as entidades relacionadas a um abrigo")
+
 
 def delete(volunteer_id):
-    print("remove uma entidade")
+    if not os.path.exists(VOLUNTEER_PATH):
+        return None
+    
+    with open(VOLUNTEER_PATH, 'r') as file:
+        volunteer_json = json.load(file)
+
+        for volunteer in volunteer_json:
+            if volunteer["id"] == volunteer_id:
+                volunteer_json.remove(volunteer)
+                break
+        
+        with open(VOLUNTEER_PATH, 'w') as file:
+            json.dump(volunteer_json, file, indent=4)
+    
+    return None
