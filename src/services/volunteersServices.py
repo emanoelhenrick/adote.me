3ca5634b4944d62f26aee4b079d8c844528954b8
FelipeLV12 <felipe.lopesv2006@gmail.com>
2025-06-02 02:50:36 -0300

    Atualização volunteersServices.py
    Melhoria no código (espero ter feito tudo certo)

--- src/services/volunteersServices.py
+++ src/services/volunteersServices.py
@@ -1,78 +1,71 @@
 from src.repositories import volunteersRepository
 from src.repositories import sheltersRepository
-from src.repositories import animalsRepository
-from src.repositories import adoptersRepository
 
 def fetchAvailableShelters():
     all_shelters = sheltersRepository.readAll()
-    available_shelters = []
-
-    if all_shelters:
-        for shelter in all_shelters:
-            if shelter['accepting_volunteers'] is True:
-                available_shelters.append(shelter)
-    
-    return available_shelters
+    return [
+        shelter for shelter in all_shelters
+        if shelter.get('accepting_volunteers') is True
+    ]
 
 
 def fetchActiveShelters(volunteer_id):
     volunteer = volunteersRepository.readById(volunteer_id)
-    active_shelters_list = []
+    if not volunteer:
+        return []
 
-    if volunteer:
-        for shelter_id_active in volunteer['active_volunteering']:
-            shelter = sheltersRepository.readById(shelter_id_active)
-            if shelter:
-                active_shelters_list.append(shelter)
-        
-        return active_shelters_list
+    return [
+        shelter for shelter_id in volunteer.get('active_volunteering', [])
+        if (shelter := sheltersRepository.readById(shelter_id))
+    ]
 
 
 def fetchPendingRequests(volunteer_id):
     volunteer = volunteersRepository.readById(volunteer_id)
-    pending_shelters_list = []
+    if not volunteer:
+        return []
 
-    if volunteer:
-        for shelter_id_pending in volunteer['applied_shelters']:
-            shelter = sheltersRepository.readById(shelter_id_pending)
-            if shelter:
-                pending_shelters_list.append(shelter)
-    
-    return pending_shelters_list
+    return [
+        shelter for shelter_id in volunteer.get('applied_shelters', [])
+        if (shelter := sheltersRepository.readById(shelter_id))
+    ]
 
 
 def requestVolunteer(shelter_id, volunteer_id):
     shelter = sheltersRepository.readById(shelter_id)
     volunteer = volunteersRepository.readById(volunteer_id)
 
-    if shelter and volunteer:
+    if not shelter or not volunteer:
+        return False
+
+    if volunteer_id not in shelter.get('volunteer_requests', []):
         shelter['volunteer_requests'].append(volunteer_id)
+
+    if shelter_id not in volunteer.get('applied_shelters', []):
         volunteer['applied_shelters'].append(shelter_id)
 
-        sheltersRepository.update(shelter)
-        volunteersRepository.update(volunteer)
+    sheltersRepository.update(shelter)
+    volunteersRepository.update(volunteer)
 
-        return True
-    return False
+    return True
 
 
 def cancelVolunteerRequest(shelter_id, volunteer_id):
     shelter = sheltersRepository.readById(shelter_id)
     volunteer = volunteersRepository.readById(volunteer_id)
+
     if not shelter or not volunteer:
         return False
 
-    new_requests_list = []
-    for requests_volunteer_id in shelter['volunteer_requests']:
-        if requests_volunteer_id != volunteer_id:
-            new_requests_list.append(requests_volunteer_id)
-    shelter['volunteer_requests'] = new_requests_list
+    shelter['volunteer_requests'] = [
+        v_id for v_id in shelter.get('volunteer_requests', [])
+        if v_id != volunteer_id
+    ]
 
-    new_requests_list2 = []
-    for requests_shelter_id in volunteer['applied_shelters']:
-        if requests_shelter_id != shelter_id:
-            new_requests_list2.append(requests_shelter_id)
-    volunteer['applied_shelters'] = new_requests_list2
+    volunteer['applied_shelters'] = [
+        s_id for s_id in volunteer.get('applied_shelters', [])
+        if s_id != shelter_id
+    ]
 
     sheltersRepository.update(shelter)
     volunteersRepository.update(volunteer)
@@ -84,13 +77,22 @@ def acceptVolunteerRequest(shelter_id, volunteer_id):
     shelter = sheltersRepository.readById(shelter_id)
     volunteer = volunteersRepository.readById(volunteer_id)
 
-    if volunteer_id in shelter['volunteer_requests']:
-        shelter['active_volunteering'].append(volunteer_id)
-        volunteer['applied_shelters'].remove(shelter_id)
+    if not shelter or not volunteer:
+        return False
+
+    if (
+        volunteer_id in shelter.get('volunteer_requests', []) and
+        shelter_id in volunteer.get('applied_shelters', [])
+    ):
         shelter['volunteer_requests'].remove(volunteer_id)
+        volunteer['applied_shelters'].remove(shelter_id)
+
+        if volunteer_id not in shelter.get('active_volunteering', []):
+            shelter['active_volunteering'].append(volunteer_id)
 
         sheltersRepository.update(shelter)
         volunteersRepository.update(volunteer)
 
         return True
+
     return False
